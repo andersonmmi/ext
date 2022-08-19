abf1e69a6b3e38da3c9df4da499f93ab5814e645
Aaron Anderson <aaronlynanderson@gmail.com>
2022-08-19 00:48:38 -0500

    make compatable with manifest v3

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,17 +4,16 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+//example of using a message handler from the inject scripts
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    chrome.pageAction.show(sender.tab.id);
+    sendResponse();
+  }
+);
