7d061d772a4a49659f62fdebe811c72870cf49d2
Aaron Anderson <aaronlynanderson@gmail.com>
2022-08-22 12:10:36 -0500

    squash bug

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,16 +4,13 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  console.log('Extension installed!');
 });
 
 //example of using a message handler from the inject scripts
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+chrome.runtime.onMessage.addListener((//message, sender, sendResponse//
+  ) => {
+    console.log('Extension message handler running')
   }
 );
